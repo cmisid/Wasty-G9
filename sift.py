569e36904c9f601c2a2252b5d7e5689d7e503266
Addou <ismailaddou@MacBook-Pro-de-Ismail.local>
2017-01-04 16:37:22 +0100

    adaptation_sift_pour_API

--- sift.py
+++ sift.py
@@ -1,29 +1,58 @@
+#Groupe [9]
+#
+
 import os
 import cv2
 import json
 
 
-sift = cv2.xfeatures2d.SIFT_create()
-descripteurs = []
-images = []
-i = 0
-for dirname, dirnames, filenames in os.walk("/Users/ismailaddou/Wasty-ImageClassifier/BD_image_pro"):
-    for filename in filenames:
-        i = i + 1
-        path = os.path.join(dirname,filename) 
-        img1 = cv2.imread(path,0) 
-        kp, des = sift.detectAndCompute(img1,None)
-        image = dict()
-        image = {
-                     'indice' : i,
-                     'category' : dirname,
-                     "desc_sift" : des1.tolist()
-                 }
-        images.append(image)
+#Entrée : url d'une image
+#Description : prend une image en entrée et retourne le descripteur associé
+#Sortie :
+def sift_descriptor(image_url):
+    sift = cv2.xfeatures2d.SIFT_create()
+    keypoints, descripteur = sift.detectAndCompute(image,None)
+    image = dict()
+    image = {
+                 'indice' : i,
+                 'category' : None,
+                 "desc_sift" : descripteur.tolist()
+             }
+    category = None
+    # classification code
 
-#print(descripteurs)
+    # ---------
+    image['category'] = category
+    return image
 
-output = json.dumps(images)
-output = json.loads(output)
-with open('descriptors.json', 'w') as outfile:
-    json.dump(output, outfile)
+#Entrée :
+#Description :
+#Sortie :
+def generate_train_descriptors():
+    sift = cv2.xfeatures2d.SIFT_create()
+    descripteurs = []
+    images = []
+    i = 0
+    #Browsing image files
+    for dirname, dirnames, filenames in os.walk("/Users/ismailaddou/Wasty-ImageClassifier/BD_image_pro"):
+        for filename in filenames:
+            i = i + 1
+            path = os.path.join(dirname,filename)
+            category = dirname.split('/')
+            category = category[len(category)-1]
+            img1 = cv2.imread(path,0)
+            #kp: keypoints, des: descriptors
+            kp, des = sift.detectAndCompute(img1,None)
+            image = dict()
+            image = {
+                         'indice' : i,
+                         'category' : category,
+                         "desc_sift" : des.tolist()
+                     }
+            images.append(image)
+
+    #Output file : descriptor.json
+    output = json.dumps(images)
+    output = json.loads(output)
+    with open('descriptors.json', 'w') as outfile:
+        json.dump(output, outfile)
