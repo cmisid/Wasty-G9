700b8c8954d78d1ab00274d0ffa2239f12b199a3
MorganSeguela <morgan.seguela@gmail.com>
2017-01-09 16:11:38 +0100

    modifPre

--- pretraitement.py
+++ pretraitement.py
@@ -1,88 +1,82 @@
-import skimage,os; 
-from skimage import io,data,feature,filters;
+# -*- conding = UTF-8 -*-
+"""
+fonctions de prétraitement de l'image avant l'extraction des
+descripteurs
+
+Nous avons dans l'ordre une lecture de l'image 
+
+Un redimensionnement de l'image
+
+Et une transformation en niveau de gris
+
+"""
+
+import os
+import skimage
+from skimage import io, data, feature, filters
 from skimage.transform import resize
 from skimage.color import rgb2gray
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from scipy import ndimage as ndi
+import math
 
-# l'objectif de cette partie du prétraitement des images est de redimmensionner les images de telle sorte 
-# que toutes les images aient la même taille, mais aussi de les convertir en niveaux de gris en vue de 
-# simplifier l'algo de classification d'images
-#
-# Chacune de ces fonctions prend en entrée une image
 
 # Fonction permettant de lire une image
 def lireImage(nom_im):
-   nom_im=skimage.io.imread(nom_im); 
-   return nom_im
-    
+    """
+    Met en commun la méthode de lecture
+    d'image
+    """
+    return skimage.io.imread(nom_im)
 
-# Fonction permettant de redimmensionner une image
-def redimmensionner(nom_im):
-    img=lireImage(nom_im);
-    img_=resize(img, (100, 100))
-    return img_
-   
+def redimensionner(img, TARGETED_HEIGHT = 100, TARGETED_WIDTH = 100):
+    """
+    Redimensionner une image
 
+    TARGETED_HEIGHT correspond à la hauteur voulu
+    TARGETED_WIDTH correspond à la largeur voulu
+    """
+    # Taille de départ
+    image_height = np.shape(img)[0]
+    image_width = np.shape(img)[1]
+    # Cote limitant de l'image
+    valeur_lim = np.argmin(np.shape(img)[0:2])
+    # proportion entre la hauteur et la largeur
+    heigt_width_proportion = image_height/image_width
 
-# Fonction permettant de convertir une image en couleur en niveaux de gris
+    # Si largeur limitant
+    if not valeur_lim:
+        # Redimensionnement proportionnel
+        img_resized = resize(img, (TARGETED_HEIGHT,\
+         math.ceil(TARGETED_WIDTH * 1/heigt_width_proportion)))
+        # Découpage de l'image
+        width_center = math.ceil(np.shape(img_resized)[1]/2)
+        img_resized = img_resized[:, width_center-50:width_center+50]
+    # Si hauteur limitant
+    else:
+        # Redimensionnement proportionnel
+        img_resized = resize(img, (math.ceil(TARGETED_HEIGHT*\
+        heigt_width_proportion), TARGETED_WIDTH))
+        # Découpage
+        height_center = math.ceil(np.shape(img_resized)[0]/2)
+        img_resized = img_resized[height_center-50:height_center+50, :]
 
-def conversion_rgb_to_gray(img):
-   img=lireImage(img);
-   img_gray = rgb2gray(img)
-   return img_gray
+    return img_resized
 
+def conversionRgbToGray(img):
+    """
+    Met en commun la méthode de conversion de
+    l'image
+    """
+    return rgb2gray(img)
 
-###########################"#####Test desfonctions créées####################################
-
-#Test de la fonction lireImage
-img=lireImage('3480940213994_P.jpg') 
-skimage.io.imshow(img)
-
-
-#Test de la Fonction redimmensionner 
-nom_im='3480940213994_P.jpg'
-img_=redimmensionner(nom_im)
-skimage.io.imshow(img_)
-    
-#Test de la Fonction conversion_rgb_to_gray 
-nom_im='3480940213994_P.jpg'
-img_gray=conversion_rgb_to_gray(nom_im)
-skimage.io.imshow(img_gray)
-###################################### Detection contour image #######################
-
-
-# Compute the Canny filter for two values of sigma 3 et 5
-im=conversion_rgb_to_gray('image2.jpg')
-edges1 = feature.canny(im,sigma=3)
-edges2 = feature.canny(im, sigma=5)
-
-# display results
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3), sharex=True, sharey=True)
-
-ax1.imshow(im, cmap=plt.cm.gray)
-ax1.axis('off')
-ax1.set_title('noisy image', fontsize=20)
-
-ax2.imshow(edges1, cmap=plt.cm.gray)
-ax2.axis('off')
-ax2.set_title('Canny filter, $\sigma=3$', fontsize=20)
-
-ax3.imshow(edges2, cmap=plt.cm.gray)
-ax3.axis('off')
-ax3.set_title('Canny filter, $\sigma=5$', fontsize=20)
-
-fig.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9,
-                    bottom=0.02, left=0.02, right=0.98)
-
-
-# Filtre de Sobel
-im=conversion_rgb_to_gray('image2.jpg')
-edges = filters.sobel(im)
-skimage.io.imshow(edges)
-
-
-# Nous avons utilisé les filtres de Canny et de Sobel pour détecter les contours de nos images pour ensuite extraire le contour le plus 
-# grand; toutefois les résultats obtenus avec ces filtres ne sont pas satisfaisants si l'image est bruitée
+if __name__ == '__main__':
+    img = lireImage("image2.jpg")
+    img_redim = redimensionner(img)
+    img_grey = conversionRgbToGray(img)
+    skimage.io.imshow(img)
+    skimage.io.imshow(img_redim)
+    skimage.io.imshow(img_grey)
+    skimage.io.show()
