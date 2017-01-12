0ac5ac07dbda282fe507c13ae106c7a39b6770fe
MorganSeguela <morgan.seguela@gmail.com>
2017-01-12 13:44:47 +0100

    miseAjourPretrait

--- pretraitement.py
+++ pretraitement.py
@@ -5,9 +5,14 @@ Programme qui permet de faire ressortir les contours d´une image
 en extrayant la partie interessante de l´image
 @author: Groupe 9: Classification d´images
 """
+
+
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 from skimage import io, feature, color
+
+
 def Canny__Contours_right(img):
     """ Fonction qui prend en entree une image
         Trace les contours de l´image et la renvoie """
@@ -24,6 +29,8 @@ def Canny__Contours_right(img):
         if edges[i, j]:
             edges[i, j:] = True
     return edges
+
+
 def Canny__Contours_left(img):
     """Fonction qui prend en entree une image, qui la parcourt de gauche a droite
         et qui renvoie une partie de l´image"""
@@ -40,6 +47,8 @@ def Canny__Contours_left(img):
         if edges[i, j]:
             edges[i, :j] = True
     return edges
+
+
 def Canny__Contours_upper(img):
     """Fonction qui prend en entree une image, qui la parcourt de droite a gauche
         et qui renvoie une partie de l´image"""
@@ -56,6 +65,8 @@ def Canny__Contours_upper(img):
         if edges[i, j]:
             edges[:i, j] = True
     return edges
+
+
 def Canny__Contours_lower(img):
     """Fonction qui prend en entree une image, qui la parcourt du haut vers le bas
        et qui renvoie une partie de l´image"""
@@ -72,9 +83,12 @@ def Canny__Contours_lower(img):
         if edges[i, j]:
             edges[i:, j] = True
     return edges
+
+
 def Canny_findContours(image):
     """Fonction qui prend en entree une image, qui utilise les fonctions
     precedentes pour reconstituer l'image finale"""
+    image = scipy.misc.imresize(image, (100, 100))
     edges1 = Canny__Contours_right(image)
     edges2 = Canny__Contours_left(image)
     edges3 = Canny__Contours_lower(image)
@@ -85,9 +99,8 @@ def Canny_findContours(image):
             if not edges1[i, j] or not edges2[i, j] or not edges3[i, j] or not edges4[i, j]:
                 image[i, j, :] = 0
     return image
-
-if __name__=='__main__':
 # Test des fonctions
+if __name__ == "__main__":
     img = plt.imread('C:/Users/samba/.spyder-py3/23.jpg')
     c = Canny_findContours(img)
     io.imshow(c)
